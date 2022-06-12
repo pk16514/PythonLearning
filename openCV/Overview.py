293781d05901c89d3882d2ddc9306a596c417c50
pk16514 <prabhakar0999@gmail.com>
2022-06-12 18:56:09 +0530

    ok

--- openCV/Overview.py
+++ openCV/Overview.py
@@ -11,7 +11,7 @@ OpenCV
 OpenCV is the huge open-source library for the computer vision, machine learning, and image processing
 and now it plays a major role in real-time operation which is very important in today’s systems. By
 using it, one can process images and videos to identify objects, faces, or even handwriting of a human.
-When it integrated with various libraries, such as Numpuy, python is capable of processing the OpenCV
+When it integrated with various libraries, such as Numpy, python is capable of processing the OpenCV
 array structure for analysis. To Identify image pattern and its various features we use vector space and
 perform mathematical operations on these features.
 
